d058fc92b5472a09d179fbe2c7382d7c816b0be9
bbutl <130008370+bbutl@users.noreply.github.com>
2023-08-18 08:56:32 +1000

    .

--- IGB200 AWIC/Assets/Scripts/EventManager.cs
+++ IGB200 AWIC/Assets/Scripts/EventManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    public List<string> avaliableOccupations;
+    GameObject character;
     // Start is called before the first frame update
     void Start()
     {
